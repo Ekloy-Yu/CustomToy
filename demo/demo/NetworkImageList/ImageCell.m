264b4f0d8fcb1e6ef3e4db1e5b938c81520e2ca2
yutq <yutianqi@sinovatech.com>
2021-12-16 18:16:55 +0800

    图片加载

--- demo/demo/NetworkImageList/ImageCell.m
+++ demo/demo/NetworkImageList/ImageCell.m
@@ -35,22 +35,34 @@
 - (void)setupConstraints {
     
     [self.mainImage mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.edges.mas_equalTo(UIEdgeInsetsMake(0, 0, 10, 0));
+//        make.edges.mas_equalTo(UIEdgeInsetsMake(0, 0, 10, 0));
+        make.top.left.right.equalTo(self.contentView);
+        make.bottom.equalTo(self.contentView).offset(-10);
     }];
 }
 
 - (void)setImageUrl:(NSString *)imageUrl{
     _imageUrl = imageUrl;
-    [self.mainImage sd_setImageWithURL:[NSURL URLWithString:imageUrl] completed:^(UIImage * _Nullable image, NSError * _Nullable error, SDImageCacheType cacheType, NSURL * _Nullable imageURL) {
-        
-        if (self.delegate && [self.delegate respondsToSelector:@selector(loadFinishSDWebImage)]) {
-            [self.delegate loadFinishSDWebImage];
-        }
-//        [self.mainImage mas_updateConstraints:^(MASConstraintMaker *make) {
-//            make.edges.mas_equalTo(UIEdgeInsetsMake(0, 0, 0, 0));
-//        }];
-        
-    }];
+    
+    UIImage *webImage = [[SDImageCache sharedImageCache] imageFromMemoryCacheForKey:imageUrl];
+    if (!webImage) {
+        [[SDWebImageDownloader sharedDownloader] downloadImageWithURL:[NSURL URLWithString:imageUrl] completed:^(UIImage * _Nullable image, NSData * _Nullable data, NSError * _Nullable error, BOOL finished) {
+            
+            [[SDImageCache sharedImageCache] storeImageToMemory:image forKey:imageUrl];
+            
+            dispatch_async_on_main_queue(^{
+                
+                if (self.delegate && [self.delegate respondsToSelector:@selector(loadFinishSDWebImage)]) {
+                    [self.delegate loadFinishSDWebImage];
+                }
+                
+            });
+            
+        }];
+    }else{
+        self.mainImage.image = webImage;
+    }
+    
 }
 
 @end
